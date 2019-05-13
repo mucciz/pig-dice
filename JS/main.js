f8f5600f2b9649d1c7132ed66a9820fc7dfd9c8c
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-05-13 09:58:54 +0300

    made more changes and additions

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,12 @@ function gameStart() {
     currentScore = 0;
     gamePlaying = true;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
     document.querySelector('.col').classList.remove('winner');
     document.querySelector('.col').classList.remove('winner');
     document.querySelector('.col').classList.remove('active');
